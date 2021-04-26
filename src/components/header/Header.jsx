553d84af2aea63c9c261fd3bf8d3bdab7f7c008a
Christian Meza <chriszdeux@gmail.com>
2021-04-26 11:22:01 -0500

    working on modal menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { AiOutlineMenu as IconMenu } from 'react-icons/ai'
-
+import { AiOutlineClose as CloseIcon, AiOutlineMenu as IconMenu, AiOutlineSearch as SearchIcon } from 'react-icons/ai';
+import { IoMdArrowDropdown as DownArrow } from 'react-icons/io';
 import '../../styles/header-style.css'
 export const Header = () => {
   return (
@@ -8,6 +8,20 @@ export const Header = () => {
       <h2>LogoApp</h2>
       <span>Read Me</span>
       <IconMenu />
+      <div>
+        <CloseIcon />
+        <form action="">
+          <input type="text"/>
+          <SearchIcon />
+        </form>
+        <ul>
+          <li>API <DownArrow /> </li>
+          <li>Games <DownArrow /> </li>
+          <li>Genre <DownArrow /> </li>
+          <li>Publishers <DownArrow /> </li>
+          <li>Platforms <DownArrow /> </li>
+        </ul>
+      </div>
     </header>
   )
 }
